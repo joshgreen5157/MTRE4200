30a43f90e31542b7330593d5e23c8936acf48525
Josh Green <josh.green5157@gmail.com>
2022-02-07 20:28:10 -0500

    lab 1-4 completed

--- run.sh
+++ run.sh
@@ -1,13 +1,8 @@
-ServerNum="1"
 Container_Name="robotic"
 Container_instance="robotic-$ServerNum"
-
+Display="10.0.0.13:0.0"
 sh buildDocker.sh $Container_Name
 
 echo "Running container..."
 
-docker run -i --rm --name=robotic --mount source=my_ws,destination=/my_ws robotic
-
-# echo "Bashing into container...."
-
-#docker exec -it $Container_Name /bin/bash
+docker run -it --rm --name=robotic -e DISPLAY="10.0.0.13:0.0" --mount source=my_ws,destination=/my_ws robotic
